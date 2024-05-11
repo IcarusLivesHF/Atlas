59ec65807f1fcda79f26b664c8d5ae7c479d723a
Icarus Lives <fg92sk8r@yahoo.com>
2024-05-11 07:24:38 -0400

    SKETCH.bat

--- SKETCH.bat
+++ SKETCH.bat
@@ -1,15 +1,9 @@
 @echo off & setlocal enableDelayedExpansion
-REM echo %import%shart
-REM pause
 
 call lib\stdlib 104 50
-call lib\3x4font "Mouse   install   by   Icarus Lives"
-call lib\getMouseEXE
 
-echo %\e%[3;2H%$3x4font%%\e%[25;45HClick anywhere%\e%[10H
 :main
 
-%getmouse%
+echo %\e%[10;10H Hello World
 
-echo %mouse.x% %mouse.y% %mouse.c%
 goto :main
