d8f6d865c9f499009f9f3e2d1667306030ea6e2b
Icarus Lives <fg92sk8r@yahoo.com>
2024-05-12 19:02:19 -0400

    Add files via upload

--- lib/stdlib.bat
+++ lib/stdlib.bat
@@ -9,23 +9,47 @@ set "unload="
 %= This creates an escaped Line Feed - DO NOT ALTER       \n =%
 )
 for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a" %= \e =%
-set "\c=[2J"                                           %= \c =%
-set "\h=[2J[H"                                         %= \h =%
+set "\c=%\e%[2J"                                       %= \c =%
+set "\h=%\e%[2J%\e%[H"                                 %= \h =%
 
 <nul set /p "=%\e%[?25l"     & rem hide cursor
+
+set "@download_Font=start https://newengine.org/downloads/fonts/MxPlus_IBM_EGA_8x8.ttf"
 
 set "@32bitlimit=0x7FFFFFFF" & rem 2147483647 or (1 << 31) - 1 or 0x7FFFFFFF
 
 set "@fullScreen=(title batchfs) ^& Mshta.exe vbscript:Execute("Set Ss=CreateObject(""WScript.Shell""):Ss.AppActivate ""batchfs"":Ss.SendKeys ""{F11}"":close")"
 
-set "@resetDim=set "wid=" & set "hei=" & set "width=" & set "height=""
+set @getDim=(%\n%
+	set "wid=" ^& set "hei=" ^& set "width=" ^& set "height="%\n%
+	for /f "skip=2 tokens=2" %%a in ('mode') do if not defined hei (set /a "hei=height=%%a") else if not defined wid set /a "wid=width=%%a"%\n%
+)
 
-set "@getDim=for /f "skip=2 tokens=2" %%a in ('mode') do (if not defined hei (set /a "hei=height=%%a") else if not defined wid (set /a "wid=width=%%a"))"
-
-if "%~3" neq "" %@fullscreen%
 %@getDim%
-if "%~1" neq "" if "%~3" equ "" (
+if "%~2" neq "" (
 	set /a "wid=width=%~1", "hei=height=%~2"
 	mode %~1,%~2
 ) 2>nul
 exit /b
+
+Features:
+	%~1 = width
+	%~2 = height
+	
+	Clears environment of unnecessary variables
+	Hides cursor
+	Sets size of window and provides definitions in variables, see below
+	
+	Macros:
+		@download_Font
+		@fullScreen
+		@getDim
+	
+	Values:
+		@32bitlimit
+		wid/width
+		hei/height
+		\e = esc
+		\c = clear screen
+		\h = clear\goto 0,0
+		\n = newLine
