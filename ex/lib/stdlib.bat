053d4983a28fe57ba98493c097a02e898a3916b9
Icarus Lives <fg92sk8r@yahoo.com>
2024-05-14 21:15:23 -0400

    Add files via upload

--- ex/lib/stdlib.bat
+++ ex/lib/stdlib.bat
@@ -5,48 +5,45 @@ for /f "tokens=1 delims==" %%a in ('set') do (
 )
 set "unload="
 
-rem \n
 (set \n=^^^
-%= This creates an escaped Line Feed - DO NOT ALTER =%
+%= This creates an escaped Line Feed - DO NOT ALTER       \n =%
 )
-rem \e char
-for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a"
-rem \c
-set "\c=[2J"
-rem \h
-set "\h=[2J[H"
+for /f %%a in ('echo prompt $E^| cmd') do set "\e=%%a" %= \e =%
+set "\c=%\e%[2J"                                       %= \c =%
+set "\h=%\e%[2J%\e%[H"                                 %= \h =%
+<nul set /p "=%\e%[?25l"     & rem hide cursor
 
-set "@32bitlimit=0x7FFFFFFF"
-
+set "@32bitlimit=0x7FFFFFFF" & rem 2147483647 or (1 << 31) - 1 or 0x7FFFFFFF
 set "@fullScreen=(title batchfs) ^& Mshta.exe vbscript:Execute("Set Ss=CreateObject(""WScript.Shell""):Ss.AppActivate ""batchfs"":Ss.SendKeys ""{F11}"":close")"
-
-set "@resetDimensions=set "wid=" & set "hei=" & set "width=" & set "height=""
-
-set "@getDimensions=for /f "skip=2 tokens=2" %%a in ('mode') do (if not defined wid (set /a "wid=width=%%a") else if not defined hei (set /a "hei=height=%%a"))"
-
-if "%~3" neq "" %@fullscreen%
-
-%@getDimensions%
-
-if "%~1" neq "" if "%~3" equ "" (
-	set /a "wid=width=%~1", "hei=height=%~2" 2>nul
-	mode %~1,%~2
+set  @getDim=(%\n%
+	set "wid=" ^& set "hei=" ^& set "width=" ^& set "height="%\n%
+	for /f "skip=2 tokens=2" %%a in ('mode') do if not defined hei (set /a "hei=height=%%a") else if not defined wid set /a "wid=width=%%a"%\n%
 )
 
-rem hide cursor
-<nul set /p "=[?25l"
+%@getDim%
+if "%~2" neq "" (
+	set /a "wid=width=%~1", "hei=height=%~2"
+	mode %~1,%~2
+) 2>nul
 exit /b
 
-REM FEATURES:
-	REM empty environment of all variables except essentials.
-	REM hides cursor
-	REM sets size of window
-	REM @fullscreen MACRO
-	REM @resetDimensions MACRO
-	REM @getDimensions MACRO
-	REM @32bitlimit VALUE
-	REM wid, width, hei, height VALUES
-	REM \n = newLine
-	REM \e = esc
-	REM \c = cls
-	REM \h = cls + move cursor back to 0,0
+Features:
+	%~1 = width
+	%~2 = height
+	
+	Clears environment of unnecessary variables
+	Hides cursor
+	Sets size of window and provides definitions in variables, see below
+	
+	Macros:
+		@fullScreen
+		@getDim
+	
+	Values:
+		@32bitlimit
+		wid/width
+		hei/height
+		\e = esc
+		\c = clear screen
+		\h = clear\goto 0,0
+		\n = newLine
