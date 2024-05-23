dbbaa791a511d6ba99b90ebc03be3c11cdc652eb
Icarus Lives <fg92sk8r@yahoo.com>
2024-05-23 12:16:31 -0400

    Add files via upload

---
+++ lib/lib/util.bat
@@ -0,0 +1,17 @@
+(set \n=^^^
+%= This creates an escaped Line Feed - DO NOT ALTER =%
+)
+
+:_getAdmin
+rem %@getAdmin%
+set "getAdmin=(Net session >nul 2>&1)|| (PowerShell start """%~0""" -verb RunAs & exit /b)"
+
+:_download
+rem %@download% url file
+set @download=for %%# in (1 2) do if %%#==2 ( for /f "tokens=1-2" %%1 in ("^!args^!") do (%\n%
+	Powershell.exe -command "(New-Object System.Net.WebClient).DownloadFile('%%~1','%%~2')"%\n%
+)) else set args=
+
+:_unique
+rem %@unique%
+set "@unique=for /f "tokens=1-7 delims=/.-: " %%a in ("%date:* =% %time: =0%") do set "$unique=%%c%%a%%b%%d%%e%%f%%g""
