18ad79e4408f3076d457f5eb95237e573cfbd3e3
Icarus Lives <fg92sk8r@yahoo.com>
2024-12-31 06:53:51 -0500

    Update updater.bat

--- updater.bat
+++ updater.bat
@@ -1,14 +1,23 @@
 @echo off & setlocal enableDelayedExpansion
 
-if exist "lib\" (
-	curl -s -o "%temp%\version.txt" "https://raw.githubusercontent.com/IcarusLivesHF/Batch-Libraries/refs/heads/main/lib/version.txt"
-	for /f "usebackq delims=" %%i in ("%temp%\version.txt") do set "remoteVersion=%%i" & set "remoteVersion=!remoteVersion:.=!"
-	for /f "usebackq delims=" %%i in ("lib\version.txt")    do set "currentVersion=%%i" & set "currentVersion=!currentVersion:.=!"
+set "tmpVer=%temp%\version.txt"
+set "remVer=https://raw.githubusercontent.com/IcarusLivesHF/Batch-Libraries/refs/heads/main/lib/version.txt"
+set "gitArchieve=https://github.com/IcarusLivesHF/Batch-Libraries/archive/refs/heads/main.zip"
 
+if exist "lib\" (
+	curl -s -o "%tmpVer%" "%remVer%"
+	for /f "usebackq delims=" %%i in ("%tmpVer%") do (
+		set "remoteVersion=%%i" & set "remoteVersion=!remoteVersion:.=!")
+	for /f "usebackq delims=" %%i in ("lib\version.txt")    do (
+		set "currentVersion=%%i" & set "currentVersion=!currentVersion:.=!")
+	del /f /q "%tmpVer%"
+	
 	if !currentVersion! lss !remoteVersion! (
 		call :download_and_unpack
+	) else (
+		echo No update at this time
+		timeout /t 2
 	)
-	del /f /q "%temp%\version.txt"
 ) else (
 	mkdir lib\
 	call :download_and_unpack
@@ -16,7 +25,7 @@ if exist "lib\" (
 exit
 
 :download_and_unpack
-	curl -L -o Batch-Libraries.zip https://github.com/IcarusLivesHF/Batch-Libraries/archive/refs/heads/main.zip
+	(curl -L -o Batch-Libraries.zip %gitArchieve%) & cls
 	powershell -Command "Expand-Archive -Path 'Batch-Libraries.zip' -DestinationPath '.' -Force"
 	copy /y "Batch-Libraries-main\lib" "%cd%\lib"
 	rmdir /S /Q "Batch-Libraries-main"
